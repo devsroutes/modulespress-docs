1af3dad6816349121d0a4298eabe9929a8327c42
Saad Nadeem <xaad46@gmail.com>
2024-11-28 21:26:01 +0500

    updated doc base url.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://modulespress.devsroutes.co',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -37,7 +37,7 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          routeBasePath: '/docs', // Serve the docs 
+          routeBasePath: '/', // Serve the docs 
         },
         blog: false,
         theme: {
